a97fa1a2cf95b779b1ba499a061f2729f10812be
kalejandro <51093501+kalejandro@users.noreply.github.com>
2018-10-23 16:43:35 -0300

    Disable MongoDB auto-configuration

--- monitor-backend/src/main/java/io/github/kalejandro/monitor/monitorbackend/MonitorBackendApplication.java
+++ monitor-backend/src/main/java/io/github/kalejandro/monitor/monitorbackend/MonitorBackendApplication.java
@@ -2,8 +2,10 @@ package io.github.kalejandro.monitor.monitorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
 public class MonitorBackendApplication {
 
   public static void main(String[] args) {
